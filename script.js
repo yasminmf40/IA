e81cc9c990f8c306728a3c2f21e384fc231ac7b1
Yasmin M Freitas <108411121+yasminmf40@users.noreply.github.com>
2024-08-23 08:33:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-.
+const caixaPrincipal = document.querySelector(".caixa-princial");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]; 
